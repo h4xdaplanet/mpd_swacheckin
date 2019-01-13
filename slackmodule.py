b03aa8bd89b8d16ee8ed32189ab4ca7d691dcb8e
h4xdaplanet <kaes@khsk.net>
2019-01-13 15:20:36 -0800

    moving to rtm and modular instance

--- slackmodule.py
+++ slackmodule.py
@@ -1,16 +1,17 @@
 from slackclient import SlackClient
 from key import token
+from key import bot_token
+import time
 
-#move to OS later
+# sc = SlackClient(bot_token)
 
-sc = SlackClient(token)
+# response = sc.api_call(
+#     'chat.postMessage',
+#     channel='U7KCNM2UT',
+#     text='testing southwest2'
+# )
+# print(response)
 
-response = sc.api_call(
-    'chat.postMessage',
-    channel='U7KCNM2UT',
-    text='testing southwest2'
-)
-print(response)
 # users = list
 # user_list = sc.api_call('users.list')
 # # print(user_list)
@@ -20,3 +21,21 @@ print(response)
 # for u in user_list['members']:
 #     if not u['deleted']:
 #         print(u['id'] + ',' + u['real_name'])
+
+# api_call = sc.api_call("users.list")
+# if api_call.get('ok'):
+#     users = api_call.get('members')
+#     for user in users:
+#         print(user.get('name'))
+
+# if sc.rtm_connect(with_team_state=False):
+#     print("Successfully connected, listening for events")
+#     while True:
+#         print(sc.rtm_read())
+#
+#         time.sleep(1)
+# else:
+#     print("Connection Failed")
+
+import bot
+bot.Bot()
